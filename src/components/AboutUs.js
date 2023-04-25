f49fbd206c7c40f67e725c97002f0cc1602012b4
SProkopios <s.koponen93@gmail.com>
2023-04-25 13:53:08 +0300

    Hienosäätöä kuvien ja tekstien kanssa

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,15 +3,15 @@
 export default function AboutUs() {
     return(
         <div>
-            <h1>MEISTÄ</h1>
-            <p className="lorumlipsum">Ompun ja Ranen Kauppa on koiranvaatteita myyvä perheyritys, joka sijaitsee Aleksanterikadulla Helsingissä.
+            <h1>Meistä</h1>
+            <p className="loremipsum">Ompun ja Ranen Kauppa on koiranvaatteita myyvä perheyritys, joka sijaitsee Aleksanterikadulla Helsingissä.
                 Yrityksen perustivat Veljekset Omppu ja Rane vuonna 2023 helsingin ytimeen.
             </p>
-            <img src="about.jpg" alt="404" width="700" height="600"/>
+            <img className="pictures" src="about.jpg" alt="404" width="650" height="600"/>
             <h3>Yhteystiedot</h3>
             <p>Aleksanterikatu 11</p>
             <p>ojarkauppa@gmail.com</p>
-            <p>044-2342344234</p>
+            <p>044-23423442</p>
         </div>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@
 export default function Home() {
     return (
       <div className="App">
-        <h1>Welcome home</h1>
+        <h1>Tervetuloa Ranen ja Ompun kauppaan!</h1>
+        <img className="pictures" src="main.jpg" alt="404" width="570" height="570"/>
       </div>
     );
   }
