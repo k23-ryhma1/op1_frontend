d69a94bff7033754abc279980055de1db73f4284
SProkopios <s.koponen93@gmail.com>
2023-04-25 11:57:48 +0300

    Lisätty lorum lipsum about us osioon

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,6 +4,14 @@ export default function AboutUs() {
     return(
         <div>
             <h1>MEISTÄ</h1>
+            <p className="lorumlipsum">Ompun ja Ranen Kauppa on koiranvaatteita myyvä perheyritys, joka sijaitsee Aleksanterikadulla Helsingissä.
+                Yrityksen perustivat Veljekset Omppu ja Rane vuonna 2023 helsingin ytimeen.
+            </p>
+            <img src="about.jpg" alt="404" width="700" height="600"/>
+            <h3>Yhteystiedot</h3>
+            <p>Aleksanterikatu 11</p>
+            <p>ojarkauppa@gmail.com</p>
+            <p>044-2342344234</p>
         </div>
     );
 }
