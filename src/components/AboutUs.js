aa0b065965bf0e2709f1ec2788023d62a549c462
elinarou <elina_rouvinen@outlook.com>
2023-05-11 09:54:06 +0300

    Muokattu Meistä-sivun tekstin muotoilua

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,9 +4,11 @@ export default function AboutUs() {
     return(
         <div>
             <h1>Meistä</h1>
-            <p className="loremipsum">Ompun ja Ranen Kauppa on koiranvaatteita myyvä perheyritys, joka sijaitsee Aleksanterikadulla Helsingissä.
-                Yrityksen perustivat Veljekset Omppu ja Rane vuonna 2023 helsingin ytimeen.
-            </p>
+            <div className="center">
+                <p className="loremipsum">Ompun ja Ranen Kauppa on koiranvaatteita ja leluja myyvä perheyritys, joka sijaitsee Aleksanterikadulla Helsingissä.
+                Yrityksen perustivat veljekset Omppu ja Rane vuonna 2023 Helsingin ytimeen.
+                </p>
+            </div>
             <img className="pictures" src="about.jpg" alt="404" width="650" height="600"/>
             <h3>Yhteystiedot</h3>
             <p>Aleksanterikatu 11</p>
